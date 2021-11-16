6d77a6b7c7f8c9f1e632b7a2d66be2e77dc36477
strange <saikumargurugubelli1234@gmail.com>
2021-11-16 21:12:47 +0530

    added api and data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useEffect,useState} from 'react'
+import './App.css'
 function App() {
+  let [ApiData,setApiData] =useState([])
+  useEffect(()=>{
+  console.log(ApiData?.entries);
+      if (ApiData.entries && !ApiData.entries.length>0) {
+      fetch('https://api.publicapis.org/entries').then((res)=>res.json()
+      .then(data => setApiData(data)))
+    .catch((err)=>console.log(err))
+    }
+  },[ApiData]);
+  
+  const getkeys= data=>{
+    let rd=[]
+    if(ApiData.entries && ApiData.entries.length>0) rd=[...Object.keys(ApiData.entries[0])]
+    console.log(rd);
+    return rd
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <h1>Products</h1>
+      <br/>
+      <div
+      className='tableclass'
+      >
+      <table
+      cellSpacing='2' 
+      style={{
+        // border:'1px solid black'
+      }}>
+        {console.log(getkeys())}
+        <tr style={{
+        border:'1px solid black'
+      }} >{getkeys().map(key=><th style={{
+          justifyContent:'center',
+          marginBottom:'1em'
+          
+        }} > {key}</th>) }</tr>
+        {ApiData.entries&& Array.isArray(ApiData.entries) && ApiData.entries.map(entry=>
+          <tr>
+              {getkeys().map(indkey=><td>{entry[indkey]}</td>)}
+            </tr>
+          )} 
+      </table>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
